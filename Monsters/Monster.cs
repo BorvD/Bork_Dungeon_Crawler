4099bd9d5986eebc8f3d5f79c4e5b05f7ecab8cd
BorvD <xenomorph_85@hotmail.com>
2025-10-17 11:23:37 +0200

    Wandering monster created

---
+++ Monsters/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bork_Dungeon_Crawler.Monsters
+{
+    internal class Monster
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int PowerLevel { get; set; }
+
+        public Monster(string name, string description, int powerLevel)
+        {
+            Name = name;
+            Description = description;
+            PowerLevel = powerLevel;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Power Level: {PowerLevel}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Monsters/WanderingMonster.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bork_Dungeon_Crawler.Monsters
+{
+    internal class WanderingMonster
+    {
+        private List<Monster> monsters = new List<Monster>();
+
+        public WanderingMonster()
+        {
+            monsters.Add(new Monster("Zombie", "A brutish warrior that loves battle.", 2));
+            monsters.Add(new Monster("Specter", "A ghostly figure that drains life energy.", 3));
+            monsters.Add(new Monster("Imp", "A mischievous fire demon, small but dangerous.", 1));
+        }
+
+        public void ShowAllMonsters()
+        {
+            Console.WriteLine("Wandering Monsters:");
+            Console.WriteLine("-------------------");
+            foreach (var monster in monsters)
+            {
+                monster.DisplayInfo();
+            }
+        }
+
+        public Monster GetRandomMonster()
+        {
+            Random rand = new Random();
+            int index = rand.Next(monsters.Count);
+            return monsters[index];
+        }
+
+        public void StartEncounter()
+        {
+            ShowAllMonsters();
+
+            Console.WriteLine("A random monster appears!");
+            Monster random = GetRandomMonster();
+            random.DisplayInfo();
+        }
+    }
+}
